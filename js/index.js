83232d245dc15de52c7b2f579d29d669375327bd
wendel isaac <wendel.isaac12@gmail.com>
2021-12-20 13:40:25 -0300

    commit

---
+++ js/index.js
@@ -0,0 +1,171 @@
+sociosDetalhes = [
+    {
+        "foto":"url('../images/socios/Pedro-Romeiro.png')",
+        "nome":"Pedro Romeiro Hermeto",
+        "area":"Negócios Imobiliários e Contencioso Estratégico",
+        "email":"pedro.romeiro@rhomadvogados.com.br",
+        "experiencia": "Pedro Romeiro Hermeto conta com mais de 45 anos de experiência, sendo admirado e respeitado na assessoria jurídica de clientes nacionais e estrangeiros em operações imobiliárias e por sua atuação no contencioso estratégico, envolvendo litígios imobiliários de alta complexidade e conflitos cíveis, em especial, relacionados a contratos administrativos e de infraestrutura.",
+        "formacao": "Inscrito na OAB-SP sob o nº 42.860, Pedro é bacharel em Direito pela Faculdade de Direito da Universidade de São Paulo (USP), em 1975, pós-graduado em Direito Processual Civil, pela Faculdade de Direito das Faculdades Metropolitanas Unidas, em 1976, e em Direito Empresarial pela Pontifícia Universidade Católica de São Paulo (PUC-SP), em 1977."
+    },
+    {
+        "foto":"url('../images/socios/Daniel-Ostronoff.png')",
+        "nome":"Daniel Ostronoff",
+        "area":"Negócios Imobiliários, Direito Registral e Contratos",
+        "email":"daniel.ostronoff@rhomadvogados.com.br",
+        "experiencia": "Daniel Ostronoff é reconhecido por sua experiência de mais de 20 anos na assessoria de clientes nacionais e estrangeiros em contratos públicos e privados, em negócios imobiliários, contratuais e societários, direito registral, e na regularização fundiária de imóveis urbanos e rurais e de loteamentos, bem como na organização e planejamento patrimonial e sucessório imobiliário. Daniel tem vasta experiência na estruturação de empreendimentos imobiliários complexos e operações correlatas, tais como, incorporações, loteamentos, condomínios e de espaços para coworking.",
+        "formacao": "Inscrito na OAB-SP sob o nº 192.980, Daniel é bacharel em Direito pela Faculdade de Direito das Faculdades Metropolitanas Unidas, em 2000, pós-graduado em Direito Contratual pela Pontifícia Universidade Católica de São Paulo (PUC-SP), em 2003, e LL.M. em Direito Contratual pelo Instituto de Estudo e Pesquisa (Insper), em 2016, tendo sido, ainda, membro efetivo da Coordenadoria de Locação, Shopping Center e Compartilhamento de Espaços da Comissão de Direito Imobiliário da OAB-SP, no período de 2019/2021, e membro convidado da Comissão de Direito Imobiliário da OAB-SP, no período de 2018/2019."
+    },
+    {
+        "foto":"url('../images/socios/Fábio-MAchado.png')",
+        "nome":"Fábio Machado Malagó",
+        "area":"Contencioso Estratégico e Negócios Imobiliários",
+        "email":"fabio.malago@rhomadvogados.com.br",
+        "experiencia": "Fábio Machado Malagó é reconhecido por sua larga experiência em direito processual civil, assessorando clientes nacionais e estrangeiros em litígios imobiliários de alta complexidade e conflitos cíveis, em especial, relacionados a contratos públicos e privados, cíveis e comerciais. Com mais de 20 anos de experiência, Fábio possui notório conhecimento em negócios imobiliários, com ênfase em locações complexas, incluindo operações de buit to suit, sale and leaseback e na assessoria na securitização de recebíveis imobiliários.",
+        "formacao": "Inscrito na OAB-SP sob o nº 236.033, Fábio é bacharel em Direito pela Faculdade de Direito da Universidade de São Paulo (USP), em 2004, pós-graduado em Licitações e Contratos Administrativos, pela Sociedade Brasileira de Direito Público (SBDP), em 2005, pós-graduado em Direito Processual Civil, pela Fundação Getúlio Vargas (GVLaw), em 2010, e mestre em Direito Processual Civil, pela Universidade de São Paulo (USP), em 2015, com a dissertação Distribuição dinâmica do ônus da prova, transformada em livro, publicado pela Editora Dialética."
+    },
+    {
+        "foto":"url('../images/socios/André-Della.png')",
+        "nome":"André Della Togna",
+        "area":"Negócios Imobiliários",
+        "email":"andre.dellatogna@rhomadvogados.com.br",
+        "experiencia": "André Della Togna possui experiência no assessoramento de clientes nacionais e estrangeiros em operações imobiliárias estruturadas, auxiliando no desenvolvimento de estruturas societárias e redes contratuais complexas, inclusive para lastrear a emissão de valores mobiliários, além de possuir experiência em questões de direito notarial e registral, principalmente em casos envolvendo incorporações imobiliárias, instituições de condomínio e regularizações imobiliárias.",
+        "formacao": "Formado pela Faculdade de Direito da Universidade de São Paulo (USP), em 2016, André atua na área de negócios imobiliários desde 2013, tendo previamente integrado o escritório de advocacia TozziniFreire Advogados. André é inscrito na OAB-SP sob o nº 389.838 e integra a comissão de Societário e Mercado de Capitais do Instituto Brasileiro de Direito Imobiliário (IBRADIM)."
+    },
+    {
+        "foto":"url('../images/socios/Eric-Matheus.png')",
+        "nome":"Eric Matheus Hadad Souza",
+        "area":"Negócios Imobiliários",
+        "email":"eric.matheus@rhomadvogados.com.br",
+        "experiencia": "Eric tem ampla experiência na assessoria de clientes nacionais e estrangeiros em operações imobiliárias complexas, incluindo a aquisição de imóveis rurais por estrangeiros, recuperação de créditos bancários e regularização de loteamentos. Atuou, ainda, em diversos setores da economia, diretamente em operações relacionadas a financiamento de projetos de infraestrutura para a construção de portos, aeroportos, minas, redes de telecomunicação, rodovias, usinas hidrelétricas, termoelétricas e eólicas.",
+        "formacao": "Formado pela Pontifícia Universidade Católica de São Paulo (PUC-SP), onde foi professor assistente de direito civil por 2 anos, Eric atua na área de negócios imobiliários desde 2015, tendo previamente integrado a banca de Machado Meyer Advogados. Eric é inscrito na OAB-SP sob o nº 426.826 e integra a comissão de Direito Urbanístico do Instituto Brasileiro de Direito Imobiliário (IBRADIM)."
+    },
+    {
+        "foto":"url('../images/socios/Matheus-Emiliano.png')",
+        "nome":"Matheus Emiliano",
+        "area":"Negócios Imobiliários",
+        "email":"matheus.emiliano@rhomadvogados.com.br",
+        "experiencia": " ",
+        "formacao": "Formado pela Pontifícia Universidade de São Paulo (PUC-SP) em 2021, Matheus atua na área de negócios imobiliários desde 2017."
+    }
+]
+
+areasDetalhes = [
+    {
+        'titulo':'Consultivo e Contratual Imobiliário',
+        'descricao': `
+            <ul>
+                <li>Assessoria jurídica no desenvolvimento de operações contratuais e imobiliárias</li>
+                <li>Realização de auditoria legal imobiliária (due diligence), para apuração da viabilidade legal de operações imobiliárias urbanas e rurais, envolvendo o imóvel e a cadeia dominial</li>
+                <li>Estruturação de empreendimentos imobiliários e operações correlatas, tais como, incorporações, venda e compra, locações de imóveis residenciais, comerciais, shoppings centers, empreendimentos hoteleiros, contratos de locação built to suit, arrendamentos, direito de superfície, desapropriações, servidões e retrocessões</li>
+                <li>Estruturação de empreendimentos imobiliários complexos, como loteamentos e condomínios </li>
+                <li>Estruturação de contratos de construção e de prestação de serviços, tendo por objeto empreendimentos residenciais, comerciais ou projetos de infraestrutura imobiliária</li>
+                <li>Administração de contratos de construção</li>
+                <li>Assessoria jurídica a investidores que tenham interesse em participar de negócios ou empreendimentos imobiliários</li>
+                <li>Estruturação de contratos cíveis e comerciais</li>
+                <li>Assessoria jurídica na estruturação de empreendimentos imobiliários de economia compartilhada, a exemplo dos espaços para Coworking, Hubs de inovação e Time Sharing</li>
+                <li>Assessoria na formatação de contratos bancários perante instituições bancárias públicas e privadas, inclusive, junto ao Banco Nacional de Desenvolvimento Econômico e Social (BNDES)</li>
+                <li>Assessoria na constituição e execução de garantias contratuais (caução, fiança, penhor, hipoteca e alienação fiduciária em garantia) e seguros em contratos imobiliários e de obras</li>
+            </ul>
+        `
+    },
+    {
+        'titulo':'Administrativo Imobiliário',
+        'descricao': `
+            <ul>
+                <li>Assessoria na aquisição e vinculação de Certificados de Certificados de Potencial Adicional de Construção (CEPAC), e outorgas onerosas objetivando acréscimo de potencial construtivo em empreendimentos imobiliários</li>
+                <li>Assessoria jurídica para a revisão de áreas de Marinha, junto à Secretaria de Patrimônio da União (SPU)</li>
+                <li>Regularização Administrativa de imóveis urbanos e rurais, loteamentos e condomínios, envolvendo imóveis particulares ou públicos, promovendo as medidas cabíveis</li>
+            </ul>
+        `
+    },
+    {
+        'titulo':'M&A e Societário Imobiliário',
+        'descricao': `
+            <ul>
+                <li>Assessoria jurídica em operações de fusão e aquisição (M&A) de sociedades imobiliárias</li>
+                <li>Estruturações societárias para a participação em operações imobiliárias, através de sociedades limitadas e anônimas, sociedades de propósito específico (SPE), sociedades em conta de participação (SCP), parcerias e consórcios, envolvendo o preparo de todos os documentos societários e/ou contratuais (Estatutos, Atas, Contratos Sociais, Acordo de Sócios ou de Acionistas etc.)</li>
+            </ul>
+        `
+    },
+    {
+        'titulo':'Planejamento Patrimonial e Sucessório Imobiliário',
+        'descricao': `
+            <ul>
+                <li>Assessoria jurídica em operações de fusão e aquisição (M&A) de sociedades imobiliárias</li>
+                <li>Estruturações societárias para a participação em operações imobiliárias, através de sociedades limitadas e anônimas, sociedades de propósito específico (SPE), sociedades em conta de participação (SCP), parcerias e consórcios, envolvendo o preparo de todos os documentos societários e/ou contratuais (Estatutos, Atas, Contratos Sociais, Acordo de Sócios ou de Acionistas etc.)</li>
+            </ul>
+        `
+    },
+    {
+        'titulo':'Direito Urbanístico e Registral',
+        'descricao': `
+            <ul>
+                <li>Assessoria jurídica em questões envolvendo operações urbanísticas e de uso e ocupação do solo (zoneamento)</li>
+                <li>Assessoria jurídica em direito registral envolvendo procedimentos de retificação de área urbana e rural (certificação e georreferenciamento), retificação de registro, desdobro e remembramento (unificação) de lotes, regularização registral e assessoria em procedimentos de dúvida registral Planejamento Patrimonial e Sucessório Imobiliário</li>
+            </ul>
+        `
+    },
+    {
+        'titulo':'Contencioso Cível e Imobiliário',
+        'descricao': `
+            <ul>
+                <li>Atuação em conflitos judiciais e arbitrais envolvendo contratos e questões cíveis e imobiliárias com a promoção ou elaboração de defesa em ações de despejo, renovatórias, revisionais, possessórias, usucapião, além daquelas que envolvem a instituição de servidões, desapropriações, ações civis públicas envolvendo questões imobiliárias, medidas acautelatórias e ações indenizatórias</li>
+            </ul>
+        `
+    },
+    {
+        'titulo':'Tributário Imobiliário',
+        'descricao': `
+            <ul>
+                <li>Assessoria jurídica tributária com relação a impostos imobiliários: IPTU, ITBI, ITCMD e Impostos de Renda sobre ganho de capital (lucro imobiliário), incluindo a recuperação de créditos</li>
+                <li>Estruturação de projetos de planejamento sucessório, com a elaboração de estruturas personalizadas de acordo com as necessidades da família</li>
+                <li>Assessoria na realização de Inventários e partilhas de bens (inventários, arrolamentos, declarações de ausência, legados e testamentos), incluindo o planejamento tributário sucessório</li>
+            </ul>
+        `
+    },
+]
+
+socios = document.querySelectorAll('.socio')
+socios.forEach((socio,index) => {
+  socio.addEventListener('click', event => {
+    document.getElementById('socios').classList.add('d-none')
+    document.getElementById('socio-detalhes').classList.add('d-flex')
+
+    sociosDetalhes.forEach((detalhe, indexDetalhes) => {
+        if(index == indexDetalhes){
+            document.getElementById('foto-detalhes').style.backgroundImage = detalhe.foto
+            document.getElementById('nome-detalhes').innerHTML = detalhe.nome
+            document.getElementById('area-detalhes').innerHTML = detalhe.area
+            document.getElementById('experiencias-detalhes').innerHTML = detalhe.experiencia
+            document.getElementById('formacao-detalhes').innerHTML = detalhe.formacao
+        }
+        
+    })
+  })
+})
+
+function fecharDetalhes(){
+    document.getElementById('socios').classList.remove('d-none')
+    document.getElementById('socio-detalhes').classList.remove('d-flex')
+}
+
+areas = document.querySelectorAll('.areas-artigo')
+
+areas.forEach((area, index) => {
+    area.addEventListener('click', event => {
+        document.getElementById('areas-atuacao-artigos').classList.add('d-none')
+        document.getElementById('areas-atuacao-detalhes').classList.add('d-flex')
+
+        areasDetalhes.forEach((areaDetalhe, indexArea) => {
+            if(index == indexArea){
+                document.getElementById('titulo-area').innerHTML = areaDetalhe.titulo
+                document.getElementById('descricao-area').innerHTML = areaDetalhe.descricao
+            }
+        })
+    })
+})
+
+function fecharAreaAtuacao(){
+    document.getElementById('areas-atuacao-artigos').classList.remove('d-none')
+    document.getElementById('areas-atuacao-detalhes').classList.remove('d-flex')
+}
